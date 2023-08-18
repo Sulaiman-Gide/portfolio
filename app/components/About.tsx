a275bf8ab570c24ceed0dbf4bf563a0125b07145
Sulaiman-Gide <sulaimanibrahimgide871@gmail.com>
2023-08-18 20:46:18 +0100

    commit

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -9,7 +9,7 @@ export default function About({}: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className='h-screen flex flex-col about relative text-center md:text-left md:flex-row max-w-7xl px-2 md:px-10 justify-evenly mx-auto items-center'>
+        className='flex flex-col about relative text-center lg:text-left lg:flex-row max-w-7xl px-2 py-10 md:px-10 justify-evenly mx-auto items-center'>
             
         <motion.img 
             initial={{
@@ -22,10 +22,10 @@ export default function About({}: Props) {
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
             src='/IMG_4462.JPG'
-            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg sm:w-54 sm:h-95 md:w-54 md:h-95 xl:w-[500px] xl:h-[600px]'
+            className='lg:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover lg:rounded-lg sm:w-54 sm:h-95 md:w-54 md:h-95 xl:w-[400px] xl:h-[500px]'
         />
-        <div className='space-y-8 md:px-10 px-2 aboutText'>
-            <h4 className='text-4xl font-semibold'>Here is a  little background</h4>
+        <div className='md:px-10 px-1'>
+            <h1 className='font-bold text-gray-300 py-5 w-full text-center uppercase tracking-[15px] lg:text-3xl text-xl'>Little background</h1>
             <p className='text-base text-gray-200 lg:text-xl'>
                 I'm Sulaiman Ibrahim Gide, here's a little bit about me... I've been coding for over 5 years now. As a Front-End developer
                 I've designed many websites and worked with startups and large corporations to help build scale thier companies.
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div className='flex top-0 p-5 items-start justify-between sticky mx-auto max-w-7xl z-50 xl:items-center navbar'>
+    <div className='flex top-0 py-5 px-5 items-start justify-between sticky mx-auto max-w-7xl z-50 xl:items-center navbar'>
         <motion.div
           initial={{
             x: -500,
@@ -21,10 +21,10 @@ function Navbar() {
             duration: 0.2,
           }}
           className='flex flex-row items-center'>
-            <SocialIcon network="youtube" fgColor="gray" bgColor="transparent" />
-            <SocialIcon network="twitter" fgColor="gray" bgColor="transparent" />
-            <SocialIcon  fgColor="gray" bgColor="transparent" />
-            <SocialIcon network="facebook" fgColor="gray" bgColor="transparent" />
+            <SocialIcon target="_blank" url="https://www.youtube.com/" network="youtube" fgColor="gray" bgColor="transparent" />
+            <SocialIcon target="_blank" url="https://twitter.com/Dev_Gide" network="twitter" fgColor="gray" bgColor="transparent" />
+            <SocialIcon target="_blank" url="https://www.instagram.com/sulaiman_gide/" network="instagram" fgColor="gray" bgColor="transparent" />
+            <SocialIcon target="_blank" url="https://web.facebook.com/" network="facebook" fgColor="gray" bgColor="transparent" />
         </motion.div>
         <motion.div
           initial={{
@@ -40,8 +40,8 @@ function Navbar() {
           transition={{
             duration: 0.2,
           }}
-          className='flex flex-row items-center text-gray-300 cursor-pointer'>
-            <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+          className='flex flex-row items-center cursor-pointer'>
+            <SocialIcon url="#contact" network="email" fgColor="gray" bgColor="transparent" />
             <Link href="#contact" >
               <button className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</button>
             </Link>
