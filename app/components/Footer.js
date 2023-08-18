91977456ba8c6e9225c99a862041b2aed5ccbd83
Sulaiman-Gide <sulaimanibrahimgide871@gmail.com>
2023-08-18 21:27:57 +0100

    first commit

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className='flex items-center justify-center flex-col'>
         <img className='relative rounded-full h-10 w-10 mx-auto object-cover' src='/IMG_0490.JPG' alt='' />
-        <h1 className="pt-3 pb-4 font-medium text-gray-200">Copyright © Dev_Gide 2023</h1>
+        <h1 className="pt-3 pb-4 font-medium">Copyright © Dev_Gide 2023</h1>
     </div>
   )
 }
