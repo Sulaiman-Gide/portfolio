aa04f0534b28583b41b7d6a837c5ee52ca344afa
Sulaiman-Gide <sulaimanibrahimgide871@gmail.com>
2023-10-12 19:13:23 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Sulaiman Gide Portfolio',
+  title: 'Sulaiman Gide',
   description: 'Sulaiman Gide Portfolio Website',
 }
 
